8058979aa0502dc985baf532e8646ab9417056f3
Gerardo Rapeta <g3r4rdo.r@gmail.com>
2024-03-15 19:09:47 -0700

    built sessions_controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    has_secure_password
+
+    validates :password, length: { minimum: 8, message: "must be at least 8 characters long" }
+    validates :email, presence: true, uniqueness: { message: "An account with this email already exists" }, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/ }
+    validates :username, presence: true, uniqueness: true
 end
