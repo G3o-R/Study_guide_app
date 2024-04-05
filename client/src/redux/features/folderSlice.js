1f4dbe58cd0e58196f517208ba17307df661853a
Gerardo Rapeta <g3r4rdo.r@gmail.com>
2024-04-04 19:54:46 -0700

    added createFolders to folderSlice

--- client/src/redux/features/folderSlice.js
+++ client/src/redux/features/folderSlice.js
@@ -2,6 +2,49 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 export const createFolder = createAsyncThunk("folder/createFolder", async (folderObjData, thunkAPI) => {
     try {
-        // const response = await fetch ("/")
-    } catch {}
+        const response = await fetch ("/folders", {
+            method: "Post",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(folderObjData)
+        })
+
+        if (!response.ok){
+            const errorMessage = await response.json()
+            return thunkAPI.rejectWithValue(errorMessage.errors)
+        }
+
+        return response.json()
+
+    } catch(errors) { 
+        return thunkAPI.rejectWithValue("An error occurred while logging in")
+     }
+})
+
+const folderSlice = createSlice({
+    name: "folder",
+    initialState: {
+        folders: [],
+        loading: false,
+        errors: []
+    },
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+        // creates folders
+        .addCase(createFolder.pending, (state) => {
+            state.loading = true;
+            state.errors = [];
+        })
+        .addCase(createFolder.fulfilled, (state, action) => {
+            state.loading = false;
+            state.folders = [...state.posts, action.payload];
+            state.errors = [];
+        })
+        .addCase(createFolder.rejected, (state, action) => {
+            state.loading = false;
+            state.errors = action.payload;
+        })
+    }
 })
