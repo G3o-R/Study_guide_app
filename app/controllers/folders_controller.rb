2d843d62ce2ba4f48672299431e51073abd36e88
Gerardo Rapeta <g3r4rdo.r@gmail.com>
2024-04-04 19:36:47 -0700

    added create route to backend

--- app/controllers/folders_controller.rb
+++ app/controllers/folders_controller.rb
@@ -1,2 +1,7 @@
 class FoldersController < ApplicationController
+
+    def create
+        new_folder = @current_user.folders.create!(folder_params)
+        render json: post, status: :created
+    end
 end
