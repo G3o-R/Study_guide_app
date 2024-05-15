5eb6a6ef0f5bbf88b38ba41c022f53239058298d
Gerardo Rapeta <g3r4rdo.r@gmail.com>
2024-05-15 16:19:59 -0700

    reactively setting width almost works but it gives me a better outline

--- client/src/components/PDFasImage.js
+++ client/src/components/PDFasImage.js
@@ -1,5 +1,4 @@
-import React, { useCallback, useState, useRef, useEffect } from 'react';
-// import { useResizeObserver } from '@wojtekmaj/react-hooks';
+import React, { useState, useRef, useEffect } from 'react';
 import styled from 'styled-components';
 import { pdfjs, Document, Page } from 'react-pdf';
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
@@ -12,10 +11,6 @@ const options = {
   standardFontDataUrl: '/standard_fonts/',
 };
 
-const resizeObserverOptions = {};
-
-const maxWidth = 800;
-
 const PDFContainer = styled.div`
     border: 1px solid #dfdfdf;
     cursor: pointer;
@@ -24,30 +19,35 @@ const PDFContainer = styled.div`
 export default function PDFasImage({document, handlePDFSelect, isLarge}) {
   const {pdf_file} = document
   const [numPages, setNumPages] = useState(null);
-  const containerRef = useRef(null);
-  const [containerWidth, setContainerWidth] = useState(null);
-
-  const onResize = useCallback((entries) => {
-    const [entry] = entries;
-
-    if (entry) {
-      setContainerWidth(entry.contentRect.width);
-    }
-  }, []);
-
-  useEffect(() => {
-    const resizeObserver = new ResizeObserver(onResize);
-    if (containerRef.current) {
-      resizeObserver.observe(containerRef.current);
-    }
-    return () => {
-      resizeObserver.disconnect();
-    };
-  }, [containerRef, onResize]);
 
   function onDocumentLoadSuccess({ numPages: nextNumPages }) {
     setNumPages(nextNumPages);
   }
+
+//   I'll handle rendering the width of the page letter
+//   but my idea od doing so would be using a useEffect to get the width of the screen
+//   and dividing that by some given number (I don't feel like thinking har rn)
+//   and that would set the state of a variable like width so that the width of the pdf is responsive
+//   to the size of the screen. I figured I'd go this route because setting the width using css seems to be messing things up so I figured I'd go easy on myself
+//   also I can't use use { useResizeObserver } from '@wojtekmaj/react-hooks'; for some reason
+
+const [width, setWidth] = useState(window.innerWidth)
+
+function handleResize(){
+  setWidth(window.innerWidth)
+}
+
+  useEffect(()=>{
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize)
+    }
+  },[])
+
+
+  console.log(width)
+
 
   return (
     <PDFContainer onClick={isLarge ? null : () => handlePDFSelect(document)} className='pdf-container'>
@@ -56,7 +56,8 @@ export default function PDFasImage({document, handlePDFSelect, isLarge}) {
                 <Page
                 key={`page_${index + 1}`}
                 pageNumber={index + 1}
-                width={800}
+                width={width >= 1000 ? 800 : (width - 200) }
+                renderTextLayer={isLarge ? true : false }
                 />
             ))}
           </Document>
